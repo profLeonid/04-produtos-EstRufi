400c4bfe8bc0b8852e1b4349e18d31e2821afe02
ESTELA RUFINO BRITO <25203670@CFP127ALUNO.LOCAL>
2026-03-10 15:40:45 -0300

    Tentando add dois novos campos

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
 "use strict"
 
 function adicionarProduto(){
+
+    const codigo = document.getElementById(`codigoColocar`)
     const produto = document.getElementById(`produtoColocar`)
+    const quantidade = document.getElementById(`quantidadeColocar`)
     const lista = document.getElementById("listas")
-    
+    const botao = document.getElementById("buttonAdd")
+
 // o span que to criando está na memória do computador
-    const span = document.createElement("span")
+    const spanCodigo = document.createElement("span")
+    const spanProduto = document.createElement("span")
+    const spanQuantidade = document.createElement("span")
 
-    span.textContent = produto.value
-    span.className = "rounded-[30px] py-2 px-4 bg-green-200"
+    spanCodigo.textContent = codigo.value
+    spanProduto.textContent = produto.value
+    spanQuantidade.textContent = quantidade.value
 
-    lista.appendChild(span)
+    spanCodigo.className = "rounded-[30px] py-2 px-4 bg-green-200"
+    spanProduto.className = "rounded-[30px] py-2 px-4 bg-red-200"
+    spanQuantidade.className = "rounded-[30px] py-2 px-4 bg-blue-200"
 
+    lista.appendChild(spanCodigo)
+    lista.appendChild(spanProduto)
+    lista.appendChild(spanQuantidade)
+
+    codigo.value = ""
+    produto.value = ""
+    quantidade.value = ""
 }
